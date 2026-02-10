84355956ef0d594ce51417aff17974a64f5f1763
Harsh Sharma <panditharshsharma34@gmail.com>
2026-02-10 14:58:57 +0530

    Classwork of 09-02-2026

--- Expressjs-backend/middleware.js
+++ Expressjs-backend/middleware.js
@@ -1,76 +1,3 @@
-// const fs = require("fs").promises;
-// const express = require("express");
-// const app = express();
-
-// app.use(express.json());
-
-// const PORT= 8000;
-
-// app.listen(PORT, () => {
-//   console.log("Server is listening on port:8000");
-// });
-
-
-// app.use((req, res, next) => {
-//     console.log("I am a middleware 1");
-//     next();
-// });
-
-// app.use((req, res, next) => {
-//     console.log("I am a middleware 2");
-//     next();
-// });
-
-// app.use((req, res, next) => {
-//   const log = `${new Date().toLocaleString()}`
-//   fs.appendFile("log.txt", (err) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     next();
-//   })
-// });
-
-// const fileAuthMiddleware = (req, res, next) => {
-//   console.log("I am checking file access");
-//   return res.send("auth filed");
-//   // next();
-// }
-
-
-// const authMiddleware = (req, res, next) => {
-//   const user = req.user;
-//   const token = req.headers["authorization"];
-//   if (!token) {
-//     return res.status(400).json({ message: "Please provide Auth Token" });
-//   }
-//   if(token === "secrettoken") {
-//     next();
-//   }
-//   else {
-//     return res.status(401).json({ message: "Unauthorized" });
-//   }
-// }
-
-// const readStudentsFromFile = async () => {
-//   const data = await fs.readFile("./students.json", "utf-8");
-//   return JSON.parse(data || "[]");
-// };
-
-// const writeStudentsToFile = async (records) => {
-//   await fs.writeFile("./students.json", JSON.stringify(records, null, 2));
-// };
-
-
-// app.get("/students", authMiddleware, async(req, res) => {
-//     const students= await readStudentsFromFile();
-//     return res.status(200).json(students);
-// });
-
-
-
-
-
 const express = require("express");
 const fs = require("fs").promises;
 
@@ -103,10 +30,10 @@ app.use((req,res,next)=>{
     next();
 });
 
-// const fileAuthMiddleware = (req, res, next) => {
-//     console.log("I am checking file access");
-//     return res.send("Auth Failed");
-// };
+const fileAuthMiddleware = (req, res, next) => {
+    console.log("I am checking file access");
+    return res.send("Auth Failed");
+};
 
 const auth_Middleware = ((req, res, next) => {
     const token = req.header("Authorization"); 
